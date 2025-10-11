f55fe2abc38abd7e55f9a4b69c686fbedf837e6a
jerryalpaca007 <ajayjadenp@gmail.com>
2025-10-11 12:45:22 +0530

    updated

--- observation16.py
+++ observation16.py
@@ -3,5 +3,3 @@
 # to create a function to readlines from india.txt and to count and display the number of lines
 # also to count the no of times the word india occurs in it
 # write a function to read the above file line by line and display each word separated by #
-
-f=open("india.txt","r+")
